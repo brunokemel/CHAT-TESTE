5166f6ccf9e0fd67f22503ff57db3dd18c836994
Bruno Kemel <br.kemel@gmail.com>
2025-06-08 22:33:50 -0300

    lengue alterada

--- src/App.tsx
+++ src/App.tsx
@@ -19,10 +19,11 @@ export default function App() {
   const messagesEndRef = useRef<HTMLUListElement>(null);
 
   useEffect(() => {
+      // BEM-VINDO AO CHAT
     alert("Bem-vindo ao chat!");
-    // Aviso para o usuário sobre o chat
+      // Aviso para o usuário sobre a perda de mensagens
     alert("O servidor não armazenará as mensagens, elas serão perdidas ao recarregar a página.");
-    // Aviso para o usuário sobre a perda de mensagens
+    
   }, []);
 
 
