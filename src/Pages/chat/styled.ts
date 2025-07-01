5912bc702b391eab5a28ab72ec5c7f05a65ed7fa
Bruno Kemel <br.kemel@gmail.com>
2025-06-30 23:25:50 -0300

    colors error concertado

--- src/Pages/chat/styled.ts
+++ src/Pages/chat/styled.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { color } from "../../Components/Colors";
+import { color } from "../../Components/colors";
 
 export const Container = styled.div`
   max-width: 600px;
